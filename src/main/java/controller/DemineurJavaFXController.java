783dde35198dac5997a2941ce0d55af1fdf41f75
2145674 <meyoutania@gmail.com>
2023-05-10 09:50:14 -0400

    Transfert des infos du demineurJavaFXControlller dans la vue

--- src/main/java/controller/DemineurJavaFXController.java
+++ src/main/java/controller/DemineurJavaFXController.java
@@ -1,9 +1,5 @@
 package controller;
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.*;
@@ -11,29 +7,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import model.MenuNiveauDifficulte;
 import model.MenuSwitchMode;
-import view.DeminCase;
+import view.Apropos;
 import view.DemineurJavaFX;
-import javafx.scene.image.ImageView;
 import view.Segment;
 import view.Temps;
 
-import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.MouseListener;
-import java.io.IOException;
-import java.net.URL;
 
 import static javafx.application.Application.launch;
 
-public class DemineurJavaFXController extends Application implements MenuSwitchMode, MenuNiveauDifficulte {
+public class DemineurJavaFXController extends Application {
 
 //    private JFrame frame = new JFrame();
 //    private Button button = new Button();
@@ -53,22 +41,24 @@ public class DemineurJavaFXController extends Application implements MenuSwitchM
     private CheckBox pause = new CheckBox("Pause");
     private Menu help = new Menu("?");
     private MenuItem menuNouveau = new MenuItem("Nouveau");
-    private MenuItem menuAffichageEnModeJavaFX = new CheckMenuItem("Affichage en mode JavaFX");
+    private MenuItem affichageEnModeSwing = new CheckMenuItem("Affichage en mode Swing");
     CheckMenuItem menuDebutant = new CheckMenuItem("Débutant");
     CheckMenuItem menuIntermediaire = new CheckMenuItem("Intermédiaire");
     CheckMenuItem menuExpert = new CheckMenuItem("Expert");
     CheckMenuItem menuPerso = new CheckMenuItem("Personalisé");
-//    private MenuItem apropos = new MenuItem("A propos");
+    private Menu apropos = new Menu("A propos");
 //    private BoxLayout layoutPanneauHaut = new BoxLayout(panneauHaut,
 //            BoxLayout.LINE_AXIS);
 //    private Node box2; //Boxes utilisÃ©es dans le BoxLayout
 //    private Node box3;
 //    private Node box1;
 //    private Node box4;
-//    private Temps temps = new Temps(affTemps); //timer sur l'affichage
+    private Temps temps = new Temps(affTemps); //timer sur l'affichage
 
     private model.Demineur demineur = new model.Demineur();
 
+
+    Apropos app = new Apropos(new Frame(), "Démineur", true);
 
     public DemineurJavaFXController() {
     }
@@ -76,24 +66,8 @@ public class DemineurJavaFXController extends Application implements MenuSwitchM
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Démineur en mode JavaFX");
-        partie.getItems().addAll(menuNouveau, menuAffichageEnModeJavaFX, menuDebutant, menuIntermediaire, menuExpert, menuPerso);
-        menu.getMenus().add(partie);
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10));
-        gridPane.setHgap(1);
-        gridPane.setVgap(1);
-        VBox root = new VBox();
-        root.getChildren().add(menu);
-        root.getChildren().add(gridPane);
-
-        for (int ligne = 0; ligne < 15; ligne++) {
-            for (int colonne = 0; colonne < 8; colonne++) {
-                Button caseButton = new Button();
-                caseButton.setPrefSize(25, 25);
-                gridPane.add(caseButton, colonne, ligne);
-            }
-        }
-        Scene scene = new Scene(root, 500, 400);
+        DemineurJavaFX demineurJavaFX = new DemineurJavaFX();
+        Scene scene = demineurJavaFX.creationInterfaceFX();
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -104,143 +78,5 @@ public class DemineurJavaFXController extends Application implements MenuSwitchM
 
 
 
-
-
-
-    //initialises le jeux
-//    public void nouveau() {
-//        temps.cancel(); //Timer Ã  0
-//        boutonNouveau.setGraphic(sourire); //Icon par dÃ©faut du bouton
-//        demineur.setnDrapeau(0);  //rÃ©initialisation des principaux paramÃ¨tres
-//        demineur.setnCases(demineur.getHAUTEUR() * demineur.getLARGEUR());
-//        affMines.setValeur(demineur.getnMines());
-//        affTemps.setValeur(0);
-//        panneauJeux.setVisible(true); //peut Ãªtre Ã  false en raison de la pause
-//        pause.setSelected(false);
-//
-//        //Génération des mines
-//        //dans la chaine, 1=mine 0=rien
-//        //on créer le bon nombre de mines puis on complète par des cases vides jusqu'à obtenir le nombre de cases total
-//        demineur.setMines("");
-//        for (int i = 0; i < demineur.getnMines(); i++) demineur.setMines(demineur.getMines() + "1");
-//        while (demineur.getMines().length() < demineur.getHAUTEUR() * demineur.getLARGEUR()) {
-//            int i = (int) (Math.random() * (demineur.getMines().length() + 1));
-//            demineur.setMines(demineur.getMines().substring(0, i) + "0" + demineur.getMines().substring(i));
-//        }
-//
-//        //paramÃ¨tres des cases
-//        for (int i = 0; i < demineur.getHAUTEUR(); i++) {
-//            for (int j = 0; j < demineur.getLARGEUR(); j++) {
-//                demineur.getJeux()[i][j].reset();
-//                demineur.getJeux()[i][j].removeMouseListener((MouseListener) this); //nÃ©cÃ©ssaire pour Ã©viter un bug lors de l'appel de nouveau() une 2Ã¨me fois
-//                demineur.getJeux()[i][j].addMouseListener((MouseListener) this); //pour les clics!!!
-//                if (demineur.getMines().charAt(i * demineur.getLARGEUR() + j) == '1') {
-//                    demineur.getJeux()[i][j].getDeminCase().setMine(true);
-//                }
-//            }
-//        }
-//        //repaint();
-//
-//        //comptage pour chaque case du nombre de mines autour
-//        for (int i = 0; i < demineur.getHAUTEUR(); i++) {
-//            for (int j = 0; j < demineur.getLARGEUR(); j++) {
-//                if (!demineur.getJeux()[i][j].getDeminCase().isMine()) {
-//                    int n = 0;
-//                    //try car les cases n'existent pas forcÃ©ment
-//                    try {
-//                        if (demineur.getJeux()[i - 1][j - 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i - 1][j].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i - 1][j + 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i][j - 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i][j + 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i + 1][j - 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i + 1][j].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    try {
-//                        if (demineur.getJeux()[i + 1][j + 1].getDeminCase().isMine()) n++;
-//                    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-//                    }
-//                    demineur.getJeux()[i][j].getDeminCase().setChiffre(n); //on indique Ã  la case le nombre de mines
-//                }
-//            }
-//        }
-//    }
-//
-//    public void creationDuModeJavaFX() {
-//
-//        frame = new JFrame("Démineur en mode JavaFX");
-//        final JFXPanel jfxPanel = new JFXPanel();
-//        Line line = new Line(100, 10, 10, 110);
-//        frame.add(jfxPanel);
-//
-//        try {
-//            jbInit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        Platform.runLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    initFX(jfxPanel);
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        });
-//    }
-//
-//    private void initFX(JFXPanel jfxPanel) throws IOException {
-//        Scene scene = createScene();
-//        jfxPanel.setScene(scene);
-//    }
-//
-//    private static Scene createScene() throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(DemineurJavaFX.class.getResource("/view/Expert.fxml"));
-//        Parent root = fxmlLoader.load();
-//        Scene scene = new Scene(root, 600, 400);
-//        return (scene);
-//    }
-//
-    @Override
-    public void jbInit() throws Exception {
-//        int tailleX = demineur.getLARGEUR() * 16 + 20; // pour la marge
-//        int tailleY = demineur.getHAUTEUR() * 16 + 20;
-//        if (tailleX < 160) tailleX = 150; //taille minimum en largeur
-//
-//        frame.setSize(tailleX, tailleY);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-    }
-//
-//
-//    @Override
-    public void selectionnerLeNiveauDeDifficulte(int type) {
-//        if (type == 1) menuDebutant.setSelected(true);
-//        if (type == 2) menuIntermediaire.setSelected(true);
-//        if (type == 3) menuExpert.setSelected(true);
-//        if (type == 4) menuPerso.setSelected(true);
-    }
 
 }
