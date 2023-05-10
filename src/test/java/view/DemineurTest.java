1a840c41deab0d5c6a076176432a0b419b07800d
2166618 <125508905+2166618@users.noreply.github.com>
2023-05-10 08:56:55 -0400

    Début test du user story

---
+++ src/test/java/view/DemineurTest.java
@@ -0,0 +1,21 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemineurTest {
+
+    @Test
+    void etantDonneLUtilisateurJoueAuDemineur_quandLUtilisateurPerd_alorsLeBoutonSourirAfficheLEtatMort() {
+        //GIVEN
+        Demineur demineur = new Demineur(16, 30, 99, 3);
+
+        //WHEN
+        
+
+
+
+        //THEN
+    }
+}
